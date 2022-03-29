7ad73f30e2e2ed3993871001b8b3bee356665600
George-Okumu <gokumu12@gmail.com>
2022-03-29 12:49:27 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,25 +1,45 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+    // Write your algorithm here
+    // let reversedWord = word.split("").reverse().join("");
+    let reversedWord = "";
+    for (let i = word.length - 1; i >= 0; i--) {
+        reversedWord += word[i];
+    }
+    // console.log(reversedWord);
+
+    if (reversedWord === word) {
+        return true;
+    } else {
+        return false;
+    }
 }
+
+// isPalindrome("racecar");
 
 /* 
   Add your pseudocode here
+  - Reverse the word
+  - initialize an empty array to carry the reversed word
+  - use if else statement,if(word === reversedWord){return true} else{return false}
 */
 
 /*
   Add written explanation of your solution here
+  - isPalindrome receives one argument, that is string word
+  - return true if the word reads same forward and backwards, otherwise false
+  - 
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+    // add your own custom tests in here
+    console.log("Expecting: true");
+    console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+    console.log("");
 
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+    console.log("Expecting: false");
+    console.log("=>", isPalindrome("robot"));
 }
 
 module.exports = isPalindrome;
